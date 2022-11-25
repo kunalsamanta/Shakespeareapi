30d72e07ed6d791f4eb7cdd99e49247989ffed77
Kunal Samanta <kunalsamanta5@gmail.com>
2022-11-25 13:14:09 +0530

    change

---
+++ app.js
@@ -0,0 +1,27 @@
+var inputTxt = document.querySelector('#input-txt');
+var btnTranslate = document.querySelector('#btn-translate');
+var outputTxt = document.querySelector('#output-txt');
+
+const url = 'https://api.funtranslations.com/translate/shakespeare.json?text=';
+
+doTranslate = () => {
+  var toTranslate = inputTxt.value;
+  changeUrl(toTranslate);
+};
+
+changeUrl = (inputTxt) => {
+  let reqUrl = url + inputTxt;
+  translate(reqUrl);
+};
+
+translate = (reqURL) => {
+  fetch(reqURL)
+    .then((response) => response.json())
+    .then((json) => {
+      outputTxt.innerText = json.contents.translated;
+    })
+    .catch(() => alert('some error occured'));
+};
+
+// Event Listner
+btnTranslate.addEventListener('click', doTranslate);
